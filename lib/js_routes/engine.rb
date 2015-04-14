e1af8be1f1b1803898f06f90cf9d0598fd6480a7
Patrick Hogan <pbhogan@gmail.com>
2015-04-14 16:59:36 -0500

    Add support for Sprockets 3

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -3,6 +3,8 @@ class JsRoutes
     JS_ROUTES_ASSET = 'js-routes'
 
     initializer 'js-routes.dependent_on_routes', after: "sprockets.environment" do
+
+      # Sprockets 2
       if Rails.application.assets.respond_to?(:register_preprocessor)
         routes = Rails.root.join('config','routes.rb').to_s
         Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
@@ -10,6 +12,13 @@ class JsRoutes
           data
         end
       end
+
+      # Sprockets 3
+      if Rails.application.assets.respond_to?(:depend_on)
+        routes = Rails.root.join('config','routes.rb').to_s
+        Rails.application.assets.depend_on "file-digest://#{routes}"
+      end
+
     end
   end
 end
