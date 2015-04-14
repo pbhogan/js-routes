b5ae3260f46264c1e2c07e9654e2790ab65e47e7
Patrick Hogan <pbhogan@gmail.com>
2015-04-14 14:52:01 -0500

    Sprockets requires the parameter to depend_on to be an actual string.

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -4,7 +4,7 @@ class JsRoutes
 
     initializer 'js-routes.dependent_on_routes', after: "sprockets.environment" do
       if Rails.application.assets.respond_to?(:register_preprocessor)
-        routes = Rails.root.join('config','routes.rb')
+        routes = Rails.root.join('config','routes.rb').to_s
         Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
           ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
           data
